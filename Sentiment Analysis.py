4b27c50f52068ea9acc61008f724cfcc1eb0d4c4
DanielWang2029 <danielwangzt@gmail.com>
2021-11-07 22:29:42 -0600

    Update Sentiment Analysis

--- Sentiment Analysis.py
+++ Sentiment Analysis.py
@@ -5,7 +5,6 @@ import json
 import sys
 import os
 import gzip
-from tqdm import tqdm_notebook as tqdm
 import matplotlib.pyplot as plt
 from textblob import TextBlob
 from wordcloud import WordCloud
@@ -14,6 +13,7 @@ plt.style.use("fivethirtyeight")
 
 
 def load_data(link, mode='r'):
+    print("Loading data...")
     data = []
     with open(link, mode) as f:
         for line in f:
@@ -22,8 +22,14 @@ def load_data(link, mode='r'):
     return pd.DataFrame.from_dict(data)
 
 
-def get_subjectivity(text):
-    return TextBlob(text).sentiment.subjectivity
+def clean_data(df):
+    print("Cleaning data...")
+    df = df.loc[(df['verified'])]
+    df = df.drop(columns=['image', 'style', 'reviewerName', 'unixReviewTime', 'verified'])
+    for col in df.columns:
+        if np.any((df.isna()[col])):
+            df[col] = df[col].fillna("" if type(df[col][0]) is str else 0)
+    return df
 
 
 def get_polarity(text):
@@ -40,8 +46,9 @@ def analyze(rating):
 
 
 def main():
-    df = load_data()
-    print(df)
+    df = load_data('data/Video_Games.json')
+    df = clean_data(df)
+    print(df.columns)
 
 
 if __name__ == '__main__':
